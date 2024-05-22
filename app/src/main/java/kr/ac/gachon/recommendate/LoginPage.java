e61d83ae4f0373c12ba269c8563508070a317c3f
Lee Yubin <lyb110804@gmail.com>
2024-05-22 23:59:41 +0900

    add login page

---
+++ app/src/main/java/kr/ac/gachon/recommendate/LoginPage.java
@@ -0,0 +1,14 @@
+package kr.ac.gachon.recommendate;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+    }
+}
---
+++ app/src/main/java/kr/ac/gachon/recommendate/Register.java
@@ -0,0 +1,4 @@
+package kr.ac.gachon.recommendate;
+
+public class Register {
+}
