479625d3a4f0272f33f21b3fa59830918bb09578
hyunjoonkang <hyunjoonkang01@gmail.com>
2024-05-29 00:17:03 +0900

    MyTagActivity, LastDatseActivitiy

---
+++ app/src/main/java/kr/ac/gachon/recommendate/LastDatesActivity.java
@@ -0,0 +1,18 @@
+package kr.ac.gachon.recommendate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LastDatesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_last_dates);
+    }
+}
---
+++ app/src/main/java/kr/ac/gachon/recommendate/MyTagActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.gachon.recommendate;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyTagActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_tag);
+    }
+}
