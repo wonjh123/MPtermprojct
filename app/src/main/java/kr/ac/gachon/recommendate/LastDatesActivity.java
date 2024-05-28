96ae580a65f7c9b3d39b557d6f726db17d37adb5
hyunjoonkang <hyunjoonkang01@gmail.com>
2024-05-29 00:55:57 +0900

    .

--- app/src/main/java/kr/ac/gachon/recommendate/LastDatesActivity.java
+++ app/src/main/java/kr/ac/gachon/recommendate/LastDatesActivity.java
@@ -14,5 +14,16 @@ public class LastDatesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_last_dates);
+
+        TextView headerTitle = findViewById(R.id.text_header_title);
+        headerTitle.setText("지난 데이트");
+
+        Button btnArrowBack = findViewById(R.id.btn_arrow_back);
+        btnArrowBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // 현재 액티비티 종료, 이전 화면으로 돌아감
+            }
+        });
     }
 }
--- app/src/main/java/kr/ac/gachon/recommendate/MyTagActivity.java
+++ app/src/main/java/kr/ac/gachon/recommendate/MyTagActivity.java
@@ -1,14 +1,27 @@
 package kr.ac.gachon.recommendate;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MyTagActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_tag);
+
+        TextView headerTitle = findViewById(R.id.text_header_title);
+        headerTitle.setText("내 태그");
+
+        Button btnArrowBack = findViewById(R.id.btn_arrow_back);
+        btnArrowBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // 현재 액티비티 종료, 이전 화면으로 돌아감
+            }
+        });
     }
 }
