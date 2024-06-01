8fe2baa1abae521eaec3a14e0a688d1c7a746044
hyunjoonkang <hyunjoonkang01@gmail.com>
2024-06-02 03:07:53 +0900

    LastDates, HomeRecommend List View

---
+++ app/src/main/java/kr/ac/gachon/recommendate/DataProvider.java
@@ -0,0 +1,25 @@
+package kr.ac.gachon.recommendate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataProvider {
+
+    public static ArrayList<HomeFragment.RecommendDate> getRecommendDates() {
+        ArrayList<HomeFragment.RecommendDate> recommendDates = new ArrayList<>();
+        recommendDates.add(new HomeFragment.RecommendDate("1. 어쩌구 데이트", "#카페 #감성"));
+        recommendDates.add(new HomeFragment.RecommendDate("2. 어쩌구 데이트", "#바다 #노을"));
+        recommendDates.add(new HomeFragment.RecommendDate("3. 저쩌구 데이트", "2004.10.31"));
+        recommendDates.add(new HomeFragment.RecommendDate("4. 저쩌구 데이트", "2004.10.31"));
+        return recommendDates;
+    }
+
+    public static ArrayList<LastDatesActivity.LastDate> getLastDates() {
+        ArrayList<LastDatesActivity.LastDate> lastDates = new ArrayList<>();
+        lastDates.add(new LastDatesActivity.LastDate("1. 어쩌구 데이트", "#카페 #감성"));
+        lastDates.add(new LastDatesActivity.LastDate("1. 어쩌구 데이트", "#카페 #감성"));
+        lastDates.add(new LastDatesActivity.LastDate("1. 어쩌구 데이트", "#카페 #감성"));
+        lastDates.add(new LastDatesActivity.LastDate("1. 어쩌구 데이트", "#카페 #감성"));
+        return lastDates;
+    }
+}
